139fa379e834e2f36249dbc18372371a54bddc1c
unrealwork <unrealwork@gmail.com>
2017-03-19 02:59:29 +0300

    custom attribute directive

--- 02-directives/src/app/app.module.ts
+++ 02-directives/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {HighlightDirective} from "./highlight.directive";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
